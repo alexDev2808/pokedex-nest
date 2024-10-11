85d1dda0bf1c64186cffd8d18335593cc4b377a3
tauesp127-alexis <gestionti@taurus.com.mx>
2024-10-11 08:58:20 -0600

    Actualizar y eliminar

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -21,11 +21,7 @@ export class PokemonService {
       return pokemon;
     } catch (error) {
       
-      if( error.code === 11000) {
-        throw new BadRequestException(`Pokemon already exists in DB`);
-      }
-      throw new InternalServerErrorException(`Can't create Pokemon - Check server logs`);
-      
+      this.handleExceptions( error );
      
     }
     
@@ -57,11 +53,34 @@ export class PokemonService {
     return pokemon;
   }
 
-  update(id: number, updatePokemonDto: UpdatePokemonDto) {
-    return `This action updates a #${id} pokemon`;
+  async update(term: string, updatePokemonDto: UpdatePokemonDto) {
+
+    try {
+      const pokemon = await this.findOne(term);
+      if( updatePokemonDto.name ) {
+        updatePokemonDto.name = updatePokemonDto.name.toLowerCase();
+      }
+
+      await pokemon.updateOne( updatePokemonDto );
+
+      return { ...pokemon.toJSON(), ...updatePokemonDto };
+    } catch (error) {
+      
+     this.handleExceptions( error );
+     
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} pokemon`;
+  async remove(id: string) {
+    const pokemon = await this.findOne( id );
+    await pokemon.deleteOne();
+  }
+
+  private handleExceptions( error: any ) {
+    if( error.code === 11000 ) {
+      throw new BadRequestException(`Pokemon already exists in DB`);
+    }
+    throw new InternalServerErrorException(`Can't create Pokemon - Check server logs`);
+    
   }
 }
