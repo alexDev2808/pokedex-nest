6d1db92db8c5b9c322bca73bddcb319a1f9a9784
alexDev2808 <colores.2808.8000@gmail.com>
2024-10-13 11:54:24 -0600

    CustomPipe para validar datos, optimizacion al eliminar

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -72,8 +72,15 @@ export class PokemonService {
   }
 
   async remove(id: string) {
-    const pokemon = await this.findOne( id );
-    await pokemon.deleteOne();
+    // const pokemon = await this.findOne( id );
+    // await pokemon.deleteOne();
+    // const result = await this.pokemonModel.findByIdAndDelete( id );
+    const { deletedCount } = await this.pokemonModel.deleteOne({ _id: id });
+
+    if( deletedCount === 0 ) {
+      throw new BadRequestException(`Pokemon with ID "${id}" not found!`);
+    }
+    return;
   }
 
   private handleExceptions( error: any ) {
